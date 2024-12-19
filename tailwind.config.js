34155244ee9f9fbecbcd61043f789a88dcaf52f6
ekamaulanablp@DESKTOP-D5753QS <ekamaulanablp23@gmail.com>
2024-12-19 12:06:17 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,37 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}", "*.html"],
+  theme: {
+    extend: {
+      colors: {
+        'belibang-black': '#121212',
+        'belibang-grey': '#A0A0A0',
+        'belibang-light-grey': '#D0D0D0',
+        'belibang-dark-grey': '#414141',
+        'belibang-darker-grey': '#1E1E1E',
+      },
+      fontFamily: {
+        "poppins": "Poppins",
+      },
+      keyframes: {
+        slide: {
+          "0%": { transform: "translateX(0%)" },
+          "100%": { transform: "translateX(-100%)" },
+        },
+        slideToR: {
+          "0%": { transform: "translateX(-100%)" },
+          "100%": { transform: "translateX(0%)" },
+        },
+        slideToT: {
+          "0%": { transform: "translateY(0%)" },
+          "100%": { transform: "translateY(-100%)" },
+        },
+        slideToB: {
+          "0%": { transform: "translateY(-100%)" },
+          "100%": { transform: "translateY(0%)" },
+        },
+      },
+    },
+  },
+  plugins: [],
+}
